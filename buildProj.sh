177c650c3401d1c71324c9bcf031181fa3214f62
The_Gost_sniper <54660359+TheGostsniperfr@users.noreply.github.com>
2024-05-26 18:26:41 +0200

    ✅ Add QMake

---
+++ buildProj.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+usage() {
+    echo "Usage:"
+    echo "./buildProj.sh -> build project."
+    echo "./buildProj.sh clean -> clean project"
+    exit 1
+}
+
+if [ $# -eq 0 ]; then
+    qmake6
+    make -j10
+    make -j10 -C lapos-0.1.2/
+elif [ "$1" = "clean" ]; then
+    make clean
+    make clean -C lapos-0.1.2/
+    rm -rf build
+    rm -rf .qmake.stash
+else
+    usage
+fi
--- requirement.sh
+++
@@ -1,4 +0,0 @@
-# Ubuntu installation
-sudo apt install cmake python3-pip libpython3-dev
-pip3 install -U spacy
-python3 -m spacy download en_core_web_sm
