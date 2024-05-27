a915b60957b3e909bf5d4d9d1795197ad9f30e4c
The_Gost_sniper <54660359+TheGostsniperfr@users.noreply.github.com>
2024-05-27 14:35:31 +0200

    ✅ Sync

--- buildProj.sh
+++ buildProj.sh
@@ -2,21 +2,43 @@
 
 usage() {
     echo "Usage:"
-    echo "./buildProj.sh -> build project."
-    echo "./buildProj.sh clean -> clean project"
+    echo "./buildProj.sh [clean] [-j N]"
+    echo "Options:"
+    echo "  clean         Clean project."
+    echo "  -j N          Specify number of threads for make (optional)."
     exit 1
 }
 
-if [ $# -eq 0 ]; then
-    qmake6
-    make -j10
-    make -j10 -C lapos-0.1.2/
-elif [ "$1" = "clean" ]; then
+THREADS=""
+
+while [[ $# -gt 0 ]]; do
+    case $1 in
+        clean)
+            CLEAN=true
+            shift
+            ;;
+        -j)
+            if [[ -n $2 && $2 =~ ^[0-9]+$ ]]; then
+                THREADS="-j$2"
+                shift 2
+            else
+                usage
+            fi
+            ;;
+        *)
+            usage
+            ;;
+    esac
+done
+
+if [ "$CLEAN" = true ]; then
     make clean
     make clean -C lapos-0.1.2/
     #rm -rf build
     rm -rf .qmake.stash
     rm -rf Makefile
 else
-    usage
+    qmake6
+    make $THREADS
+    make $THREADS -C lapos-0.1.2/
 fi
