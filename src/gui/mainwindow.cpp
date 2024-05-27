33ab0f21df38ce5ec052da35340884d3b9009b95
The_Gost_sniper <54660359+TheGostsniperfr@users.noreply.github.com>
2024-05-27 16:44:31 +0200

    ✅ Add get values logic

--- src/gui/mainwindow.cpp
+++ src/gui/mainwindow.cpp
@@ -3,8 +3,8 @@
 
 
 MainWindow::MainWindow(QWidget *parent)
-    : QMainWindow(parent)
-    , ui(new Ui::MainWindow)
+    : QMainWindow(parent),
+    ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
 }
@@ -13,3 +13,21 @@ MainWindow::~MainWindow()
 {
     delete ui;
 }
+
+void MainWindow::on_pushButton_clicked()
+{
+    QString text = ui->inputTxt->toPlainText();
+    qInfo() << text;
+
+    // Get modifiers :
+    bool basicModifierCheckbox = ui->basicModifierCheckbox->isChecked();
+    bool urlModifierCheckbox = ui->urlModifierCheckbox->isChecked();
+    bool pontModifierCheckbox = ui->ponctModifierCheckbox->isChecked();
+    bool dateModifierCheckbox = ui->dateModifierCheckbox->isChecked();
+
+    qInfo() << "basicModifierCheckbox:" << basicModifierCheckbox;
+    qInfo() << "urlModifierCheckbox:" << urlModifierCheckbox;
+    qInfo() << "pontModifierCheckbox:" << pontModifierCheckbox;
+    qInfo() << "dateModifierCheckbox:" << dateModifierCheckbox;
+}
+
